163ffe5c90e16b7d86e88cfcc1a8bdf63733c183
TerraAM <ya.rpg2001@yandex.ru>
2024-10-12 03:12:54 +0500

    Добавление комментариев в класса ошибок (повторно)

--- src/main/java/lesson12/custom/errors/MyArrayDataException.java
+++ src/main/java/lesson12/custom/errors/MyArrayDataException.java
@@ -1,12 +1,12 @@
-package main.java.lesson12.custom.errors;
+package main.java.lesson12.custom.errors;//Определяет пакет, в котором находится файл
 
+//Класс ошибки формата данных элемента массива, наслденик от главного класса ошибок Exception
 public class MyArrayDataException extends Exception{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 9043070217069408347L;
+	
+	private static final long serialVersionUID = 9043070217069408347L;//Нужен для контроля версии класса (что бы была совместимасть даже после изменения)
 
+	//Конструктор класса
 	public MyArrayDataException(String message) {
-        super(message);
+        super(message);//Вызываем конструктор родителя
 	}
 }
