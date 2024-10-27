6f2be48ef85d205dbdbca7b45a9888acc280174c
TerraAM <51695665+TerraAM@users.noreply.github.com>
2024-10-27 22:11:47 +0500

    Убраны System.out.println в тестах класса MTSOnlinePaymentTest

--- src/test/java/lesson16/MTSOnlinePaymentTest.java
+++ src/test/java/lesson16/MTSOnlinePaymentTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -17,8 +18,8 @@ import lesson16.pages.MTSOnlinePaymentPage;
 /*Класс теста блока онлайн пополнения (mts.by)*/
 public class MTSOnlinePaymentTest {
 
-		/*Блок задания полей*/
-		private WebDriver driver;
+	    /*Блок задания полей*/
+	    private WebDriver driver;
 	    private MTSOnlinePaymentPage mtsOnlinePaymentPage;
 	    private String pathMoreInfo = "/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
 	    private String titleOnlinePayment = "Онлайн пополнение без комиссии";
@@ -49,23 +50,23 @@ public class MTSOnlinePaymentTest {
 	    }
 	
 	    //Тест проверки названия блока 'Онлайн пополнение без комиссии'
+	    @DisplayName("Проверка названия блока")
 	    @Test
 	    void checkBlockTitle() {
-	        System.out.println("\nПроверка названия блока:");
 	        assertThat(mtsOnlinePaymentPage.getBlockTitle()).isEqualTo(titleOnlinePayment);
 	    }
 	
 	    //Тест проверки наличия логотипов платежных систем
+	    @DisplayName("Проверка наличия логотипов платежных систем")
 	    @Test
 	    void checkPaymentSystemLogos() {
-	        System.out.println("\nПроверка наличия логотипов платежных систем:");
 	        assertThat(mtsOnlinePaymentPage.areAllLogosDisplayed(expectedAltTexts)).isTrue();
 	    }
 	
 	    //Тест проверка ссылки 'Подробнее о сервисе'
+	    @DisplayName("Проверка ссылки 'Подробнее о сервисе'")
 	    @Test
 	    void checkMoreInfoButton() {
-	        System.out.println("\nПроверка ссылки 'Подробнее о сервисе':");
 	        assertThat(mtsOnlinePaymentPage
 	        							   .clickMoreInfoButton()
 	        							   .getCurrentUrl())
@@ -73,9 +74,9 @@ public class MTSOnlinePaymentTest {
 	    }
 	
 	    //Тест проверки работы формы 'Услуги связи'
+	    @DisplayName("Проверка работы формы 'Услуги связи':")
 	    @Test
 	    void checkFormSubmissionForConnection() {
-	        System.out.println("\nПроверка работы формы 'Услуги связи':");
 	        assertThat(mtsOnlinePaymentPage
 	        							   .fillConnectionForm(phone, sum, email)
 	        							   .submitConnectionForm()
@@ -83,38 +84,37 @@ public class MTSOnlinePaymentTest {
 	    }
 	    
 	    //Тест проверки placeholder для формы 'Услуги связи'
+	    @DisplayName("Проверка placeholder для формы 'Услуги связи'")
 	    @Test
 	    void checkConnectionPlaceholders() {
-	        System.out.println("\nПроверка placeholder для формы 'Услуги связи':");
 	        assertThat(mtsOnlinePaymentPage.checkConnectionPlaceholders()).isTrue();
 	    }
 
 	    //Тест проверки placeholder для формы 'Домашний интернет'
+	    @DisplayName("Проверка placeholder для формы 'Домашний интернет'")
 	    @Test
 	    void checkInternetPlaceholders() {
-	        System.out.println("\nПроверка placeholder для формы 'Домашний интернет':");
 	        assertThat(mtsOnlinePaymentPage.checkInternetPlaceholders()).isTrue();
 	    }
 
 	    //Тест проверки placeholder для формы 'Рассрочка'
+	    @DisplayName("Проверка placeholder для формы 'Рассрочка'")
 	    @Test
 	    void checkInstalmentPlaceholders() {
-	        System.out.println("\nПроверка placeholder для формы 'Рассрочка':");
 	        assertThat(mtsOnlinePaymentPage.checkInstalmentPlaceholders()).isTrue();
 	    }
 
 	    //Тест проверки placeholder для формы 'Задолженность'
+	    @DisplayName("Проверка placeholder для формы 'Задолженность'")
 	    @Test
 	    void checkArrearsPlaceholders() {
-	        System.out.println("\nПроверка placeholder для формы 'Задолженность':");
 	        assertThat(mtsOnlinePaymentPage.checkArrearsPlaceholders()).isTrue();
 	    }
 	    
 	    //Тест проверки текста в незаполненных полях модального окна оплаты
+	    @DisplayName("Модальное окно оплаты. Проверка текста в незаполненных полях")
 	    @Test
 	    void checkPaymentFrameLabelsText() {
-	        System.out.println("\nМодальное окно оплаты\n"
-	        				 + "Проверка текста в незаполненных полях:");
 	        assertThat(mtsOnlinePaymentPage
 	        							   .fillConnectionForm(phone, sum, email)
 	        							   .submitConnectionForm()
@@ -122,10 +122,9 @@ public class MTSOnlinePaymentTest {
 	    }
 	    
 	    //Тест проверки отображения логотипов банковских компаний модального окна оплаты
+	    @DisplayName("Модальное окно оплаты. Проверка отображения логотипов банковских компаний:")
 	    @Test
 	    void checkPaymentFrameImg() {
-	        System.out.println("\n--Модальное окно оплаты--\n"
-	        				 + "Проверка отображения логотипов банковских компаний:");
 	        assertThat(mtsOnlinePaymentPage
 	        							   .fillConnectionForm(phone, sum, email)
 	        							   .submitConnectionForm()
@@ -133,10 +132,9 @@ public class MTSOnlinePaymentTest {
 	    }
 	    
 	    //Тест проверки отображения суммы на окне и на кнопке модального окна оплаты
+	    @DisplayName("Модальное окно оплаты. Проверка отображения суммы на окне и на кнопке:")
 	    @Test
 	    void checkPaymentFrameAmount() {
-	    	System.out.println("\n--Модальное окно оплаты--\n"
-   				 			 + "Проверка отображения суммы на окне и на кнопке:");
 	        assertThat(mtsOnlinePaymentPage
 	        							   .fillConnectionForm(phone, sum, email)
 	        							   .submitConnectionForm()
